5983aa6f4932057d0dd151fb47dc97aed8db2d1e
Vinícius Vilela <viniciusvilela19@gmail.com>
2019-03-22 16:26:31 -0300

    coverage e mock

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 const { User } = require('../models');
 
+const Mail = require('../services/MailService');
+
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
@@ -13,6 +15,13 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ message: 'Password not check' });
     }
+
+    await Mail.send({
+      from: 'Leleu Vilela <leleu@leu.com>',
+      to: `${user.name} <${user.email}>`,
+      subject: 'New Access',
+      text: 'New access as registred in your account'
+    });
 
     return res.json({
       token: await user.generateToken()
